a73136ea5a88fc6a89cbe3428efca7a8fe9a428f
Tyler Fuqua <tyler@agilityda.com>
2017-12-26 16:35:46 -0700

    Hands On Project 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// Hands On Project 1
+
+function addNumbers (num1, num2 = 10) {
+    return num1 + num2;
+}
+console.log(addNumbers(10));
+
+function addNumbers (num3, num4 = 10) {
+    return num3 * num4;
+}
+console.log(addNumbers(10));
+
+function addNumbers (num3, num4 = 10) {
+    return (num3 + num4) / 2;
+}
+console.log(addNumbers(10));
