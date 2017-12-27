5b1d91b32f97cf9f6b20683def0cfb952c217d2f
Tyler Fuqua <tyler@agilityda.com>
2017-12-26 18:33:01 -0700

    Hands On Project 2

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 // Hands On Project 1
 
-function addNumbers (num1, num2 = 10) {
+// function addNumbers (num1, num2 = 10) {
+//     return num1 + num2;
+// }
+// console.log(addNumbers(10));
+
+// function addNumbers (num3, num4 = 10) {
+//     return num3 * num4;
+// }
+// console.log(addNumbers(10));
+
+// function addNumbers (num3, num4 = 10) {
+//     return (num3 + num4) / 2;
+// }
+// console.log(addNumbers(10));
+
+// Hands On Project 2
+
+const addNumbers = (num1, num2) => {
     return num1 + num2;
 }
-console.log(addNumbers(10));
+ console.log(addNumbers(10, 10));
 
-function addNumbers (num3, num4 = 10) {
+
+
+const multiplyNumbers  = (num3, num4) => {
     return num3 * num4;
 }
-console.log(addNumbers(10));
+console.log(multiplyNumbers(10, 10));
 
-function addNumbers (num3, num4 = 10) {
+const meanNumbers  = (num3, num4) => {
     return (num3 + num4) / 2;
 }
-console.log(addNumbers(10));
+console.log(meanNumbers(10, 15));
