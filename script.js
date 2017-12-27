867f39d00ae12fe6561c7554ccdbf40333e24c3c
Tyler Fuqua <tyler@agilityda.com>
2017-12-26 18:51:50 -0700

    Hands On Project 3

--- script.js
+++ script.js
@@ -17,19 +17,41 @@
 
 // Hands On Project 2
 
-const addNumbers = (num1, num2) => {
-    return num1 + num2;
-}
- console.log(addNumbers(10, 10));
+// const addNumbers = (num1, num2) => {
+//     return num1 + num2;
+// }
+//  console.log(addNumbers(10, 10));
 
 
 
-const multiplyNumbers  = (num3, num4) => {
-    return num3 * num4;
-}
-console.log(multiplyNumbers(10, 10));
+// const multiplyNumbers  = (num3, num4) => {
+//     return num3 * num4;
+// }
+// console.log(multiplyNumbers(10, 10));
 
-const meanNumbers  = (num3, num4) => {
-    return (num3 + num4) / 2;
-}
-console.log(meanNumbers(10, 15));
+// const meanNumbers  = (num3, num4) => {
+//     return (num3 + num4) / 2;
+// }
+// console.log(meanNumbers(10, 15));
+
+// Hands On Project 3
+
+let newProduct = {
+    brand: "Nike",
+    shoes: "Air Jordans",
+    price: "$150",
+    getProduct: function () {
+        console.log(this.brand + "\n" + this.shoes + "\n" + this.price );
+    }
+};
+newProduct.getProduct();
+
+let careerPotential = {
+    career: "Software Development",
+    position: "Senior Developer",
+    salary: "80K",
+    getCareer: function () {
+        console.log(this.career + "\n" + this.position + "\n" + this.salary);
+    }
+};
+careerPotential.getCareer();
